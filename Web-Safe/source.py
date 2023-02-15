7cea9aebee7aea80ae3e16b971aa6b6d5bd4f9fe
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-02-15 21:28:12 +0530

    Update source.py
    New Update:
    
        It uses a function check_website_safety to encapsulate the website safety-checking logic.
    
        It uses a timeout of 5 seconds when sending the HTTP request to avoid the script hanging indefinitely on unresponsive websites.
    
        It checks the Content-Type header of the response to ensure that the website is actually serving HTML content.
    
        It checks the text of the response to see if it contains phrases like "Not Found" or "Error" that suggest a problem with the website.
    
        It returns a string message describing the result of the safety check, which can be printed or displayed to the user.

--- Web-Safe/source.py
+++ Web-Safe/source.py
@@ -1,14 +1,31 @@
 import requests
+from typing import Tuple
 
+def check_website_safety(url: str) -> Tuple[bool, str]:
+    """
+    Checks if a website is safe or not by sending an HTTP GET request to the URL
+    and analyzing the response. Returns a tuple containing a boolean indicating
+    whether the website is safe or not, and a string describing the result.
+    """
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == requests.codes.ok:
+            content_type = response.headers.get('Content-Type')
+            if 'text/html' in content_type:
+                if 'Not Found' in response.text or 'Error' in response.text:
+                    return (False, 'Website may be unsafe (page not found or error message)')
+                else:
+                    return (True, 'Website is safe')
+            else:
+                return (False, 'Website may be unsafe (content type is not HTML)')
+        else:
+            return (False, 'Website may be unsafe (bad response code: {})'.format(response.status_code))
+    except requests.exceptions.Timeout:
+        return (False, 'Website may be unsafe (timeout)')
+    except requests.exceptions.RequestException:
+        return (False, 'Website may be unsafe (request exception)')
+
+# Example usage
 url = input("Enter website URL: ")
-
-try:
-    response = requests.get(url)
-    if response.status_code >= 200 and response.status_code < 300:
-        print("The website is safe.")
-    elif response.status_code >= 400 and response.status_code < 500:
-        print("The website is unsafe.")
-    else:
-        print("The safety of the website could not be determined.")
-except requests.exceptions.RequestException:
-    print("The website could not be reached.")
+is_safe, message = check_website_safety(url)
+print(message)
