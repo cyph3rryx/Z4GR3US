c5d50d9d1bc84bd385fa9f4cdfef98e3b9fa08cb
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-02-20 21:54:09 +0530

    Update source.py
    This code includes two additional functions: `check_ssl_certificate()` and `check_for_malware()`. 
    
    `check_ssl_certificate()` uses the `ssl` module to verify the SSL/TLS certificate of the website. It checks if the certificate has expired and returns a boolean indicating whether the certificate is valid or not, and a message describing the result.
    
    `check_for_malware()` uses the VirusTotal API to scan the website for malware or viruses. It sends a URL to VirusTotal to scan and retrieve the scan report. If the report shows that the website has been detected as malware, the function returns a boolean indicating that the website is unsafe and a message describing the result. Otherwise, the function returns a boolean indicating that the website is safe and a message describing the result.
    
    Note that to use the VirusTotal API, you need to sign up for an API key and replace the value of `VT_API_KEY` with your own key. Also, you need to install the `virustotal-api` package to use the VirusTotal API.

--- Web-Safe/source.py
+++ Web-Safe/source.py
@@ -1,5 +1,17 @@
 import requests
+import ssl
+import datetime
 from typing import Tuple
+from googlesearch import search
+import urllib.request
+import urllib.error
+import re
+from bs4 import BeautifulSoup
+import virustotal_api
+
+#Initialize the VirusTotal API
+VT_API_KEY = 'YOUR_API_KEY'
+vt = virustotal_api.Client(API_KEY)
 
 def check_website_safety(url: str) -> Tuple[bool, str]:
     """
@@ -15,7 +27,7 @@ def check_website_safety(url: str) -> Tuple[bool, str]:
                 if 'Not Found' in response.text or 'Error' in response.text:
                     return (False, 'Website may be unsafe (page not found or error message)')
                 else:
-                    return (True, 'Website is safe')
+                    return (check_ssl_certificate(response), check_for_malware(url))
             else:
                 return (False, 'Website may be unsafe (content type is not HTML)')
         else:
@@ -24,6 +36,43 @@ def check_website_safety(url: str) -> Tuple[bool, str]:
         return (False, 'Website may be unsafe (timeout)')
     except requests.exceptions.RequestException:
         return (False, 'Website may be unsafe (request exception)')
+
+def check_ssl_certificate(response: requests.Response) -> Tuple[bool, str]:
+    """
+    Checks if the website's SSL/TLS certificate is valid.
+    Returns a tuple containing a boolean indicating whether the certificate is valid or not,
+    and a string describing the result.
+    """
+    try:
+        ssl_info = ssl.get_server_certificate((response.url, 443))
+        x509 = ssl.load_certificate(ssl_info.encode())
+        expire_date = x509.get_notAfter().decode("utf-8")
+        expire_date_obj = datetime.datetime.strptime(expire_date, '%Y%m%d%H%M%SZ')
+        if expire_date_obj < datetime.datetime.now():
+            return (False, 'Website may be unsafe (SSL certificate expired on {})'.format(expire_date_obj.strftime('%d %B %Y')))
+        else:
+            return (True, 'Website has a valid SSL certificate (expires on {})'.format(expire_date_obj.strftime('%d %B %Y')))
+    except Exception as e:
+        return (False, 'Unable to check SSL certificate: {}'.format(str(e)))
+
+def check_for_malware(url: str) -> Tuple[bool, str]:
+    """
+    Checks if the website is infected with any malware or viruses using VirusTotal API.
+    Returns a tuple containing a boolean indicating whether the website is infected or not,
+    and a string describing the result.
+    """
+    try:
+        # Scan the URL using VirusTotal API
+        scan_result = vt.scan_url(url)
+        scan_id = scan_result['scan_id']
+        # Retrieve the scan report using the scan ID
+        report = vt.get_report(scan_id)
+        if report['results']['positives'] > 0:
+            return (False, 'Website may be unsafe (detected as malware by VirusTotal)')
+        else:
+            return (True, 'Website is safe (not detected as malware by VirusTotal)')
+    except Exception as e:
+        return (False, 'Unable to check for malware: {}'.format(str(e)))
 
 # Example usage
 url = input("Enter website URL: ")
