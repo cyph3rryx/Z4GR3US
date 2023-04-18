95caf0945e3fc55736eef1b822dc596bfd25bb34
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-04-18 16:27:01 +0530

    Update logger.py
    A Logger script that includes all three advancements: file rotation, automatic startup, and remote access via Dropbox

--- Logger/logger.py
+++ Logger/logger.py
@@ -1,18 +1,36 @@
-import logging
 import os
+import logging
+import logging.handlers
+import dropbox
 
-# Define the log file location and format
-log_file = 'startup_log.txt'
-log_format = '%(asctime)s - %(levelname)s - %(message)s'
+log_filename = 'startup_log.txt'
+log_max_bytes = 1024 * 1024  # 1 MB
+log_backup_count = 5  # Keep up to 5 old log files
+dropbox_access_token = 'your-dropbox-access-token'
 
-# Set up the logging configuration
-logging.basicConfig(filename=log_file, level=logging.INFO, format=log_format)
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-# Log the startup information
-logging.info('PC started up')
+log_handler = logging.handlers.RotatingFileHandler(log_filename, maxBytes=log_max_bytes, backupCount=log_backup_count)
+log_handler.setFormatter(log_formatter)
 
-# Log the contents of the startup directory
-startup_dir = os.path.join(os.getenv('APPDATA'), 'Microsoft\\Windows\\Start Menu\\Programs\\Startup')
-logging.info('Startup directory contents:')
-for file in os.listdir(startup_dir):
-    logging.info(file)
+logger = logging.getLogger()
+logger.addHandler(log_handler)
+logger.setLevel(logging.INFO)
+
+# Add the following lines to run the script at startup
+startup_path = os.path.join(os.environ['APPDATA'], 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
+startup_script_path = os.path.abspath(__file__)
+startup_script_link_path = os.path.join(startup_path, 'Logger.lnk')
+
+if not os.path.exists(startup_script_link_path):
+    from win32com.client import Dispatch
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(startup_script_link_path)
+    shortcut.Targetpath = startup_script_path
+    shortcut.WorkingDirectory = os.path.dirname(startup_script_path)
+    shortcut.save()
+
+# Add the following lines to upload the log file to Dropbox
+with open(log_filename, 'rb') as f:
+    dbx = dropbox.Dropbox(dropbox_access_token)
+    dbx.files_upload(f.read(), '/' + log_filename)
