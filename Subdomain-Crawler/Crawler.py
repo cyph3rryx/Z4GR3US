a68bf9618029939c4a37c3366aa5f00c44c4bc98
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-09-12 19:55:30 +0530

    Update Crawler.py

--- Subdomain-Crawler/Crawler.py
+++ Subdomain-Crawler/Crawler.py
@@ -10,15 +10,24 @@ def print_loading_animation():
         print("\r" + "Gathering subdomains... " + animation[i % len(animation)], end="")
 
 def find_subdomains(domain):
-    result = subprocess.run(['subfinder', '-d', domain], stdout=subprocess.PIPE)
-    subdomains = result.stdout.decode().splitlines()
-    return subdomains
+    try:
+        result = subprocess.run(['subfinder', '-d', domain], stdout=subprocess.PIPE, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while trying to find subdomains: {str(e)}")
+        return []
+    else:
+        subdomains = result.stdout.splitlines()
+        return subdomains
 
 def save_to_file(subdomains, domain):
     filename = f"subdomains_{domain}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
-    with open(filename, 'w') as file:
-        file.write('\n'.join(subdomains))
-    print(f"Results saved to {filename}")
+    try:
+        with open(filename, 'w') as file:
+            file.write('\n'.join(subdomains))
+    except IOError as e:
+        print(f"An error occurred while trying to write to the file: {str(e)}")
+    else:
+        print(f"Results saved to {filename}")
 
 if __name__ == '__main__':
     print("Welcome to the subdomain finder program.")
